9f456ecc33f1dd4a588e7f87bf897b21a153e0fa
Sauliha <saulihashaikh86@gmail.com>
2026-08-25 22:59:00 +0530

    adding Readme.py .md ko .py kr diya

---
+++ README.py
@@ -0,0 +1,31 @@
+# python_practice
+# l=[]
+# for i in range(2000, 3201):
+#     if (i%7==0) and (i%5!=0):
+#         l.append(str(i))
+
+# print(','.join(l))
+
+
+# for x in range(2000, 3201):
+#     if(x%7==0 and x%5!=0):
+#         print(x, end=",")
+
+
+
+# a='12 342 546 767 34 565 66 88 sgin rign iors irir ogj'
+# spla=a.split()
+# print(spla)
+# print("=".join(spla))
+
+
+
+f = int(input("Enter a number: "))
+fact=1
+for x in range(f, 1, -1):
+    fact= fact*x
+    print("factorial:", fact)
+
+
+
+x=int(input("Enter number:"))
